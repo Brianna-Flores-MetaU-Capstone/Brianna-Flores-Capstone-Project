1730064ca6f7ff8a89cf16876d495b4f61a421d0
Brianna Flores <briannaflores@meta.com>
2025-06-25 10:16:59 -0700

    this commit will import react router and add boiler plate code for the homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
 // import { useState } from 'react'
 import './App.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+
+// import pages for routing
+import Homepage from './pages/Homepage';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import '../styles/Homepage.css'
+
+const Homepage = () => {
+    return (
+        <div>
+            Homepage
+        </div>
+    )
+}
+
+export default Homepage;
