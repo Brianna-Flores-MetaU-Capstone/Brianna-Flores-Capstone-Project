92e2b5eec1025f9d345b7dbe0709a7f733f10338
Brianna Flores <briannaflores@meta.com>
2025-06-26 09:56:23 -0700

    this commit will add express session for typescript compatibility

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,26 @@
+// get the types for defining req and res
+import { Request, Response } from  "express";
 const express = require('express');
+
+// modify the express session module to allow extraction of userId from session in auth.ts
+declare module 'express-session' {
+  interface SessionData {
+    userId?: string
+  }
+}
+const session = require('express-session')
 const app = express();
 const PORT = 3000;
+// takes in json format
+app.use(express.json())
 
-// import react session for safety
-const session = require('express-session')
+// allow to import data from .env file
+import dotenv from 'dotenv'; 
+dotenv.config();
 
 let sessionConfig = {
   name: 'sessionId',
-  secret: 'keep it secret, keep it safe',
+  secret: process.env.SESSION_SECRET, 
   cookie: {
     maxAge: 1000 * 60 * 5,
     secure: process.env.RENDER ? true : false,
@@ -18,10 +31,10 @@ let sessionConfig = {
 }
 
 app.use(session(sessionConfig))
-const authRouter = require('./routes/auth')
 
-// get the types for defining req and res
-import { Request, Response } from  "express";
+const authRouter = require('./routes/auth')
+app.use(authRouter)
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
@@ -30,4 +43,3 @@ app.listen(PORT, () => {
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to my app!')
 })
-
--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -1,11 +1,11 @@
+import { Request, Response } from  "express";
 const router = require('express').Router() // combines the lines const router = require('express');
 const bcrypt = require('bcrypt')
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
-import { Request, Response } from  "express";
 
-// signup route
+// Allow the user to signup
 router.post("/signup", async(req: Request, res: Response) => {
     try {
         const { username, password } = req.body
@@ -44,5 +44,56 @@ router.post("/signup", async(req: Request, res: Response) => {
         res.status(500).json({error: "Something went wrong during signup!"})
     }
 })
+
+// when a user tries to login: post request
+router.post("/login", async(req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body
+        // ensure that username and password are valid (no empty strings)
+        if (!username || !password) {
+            return res.status(400).json({error: "Username and password are required"})
+        }
+
+        // search for username in database
+        const user = await prisma.user.findUnique({
+            where: { username }
+        })
+        if (!user) {
+            return res.status(400).json({ error: "Invalid username or password." });
+        }
+        // if the user exists compare the entered password with the password associated with the username
+        const isValidPassword = await bcrypt.compare(password, user.password)
+        if (!isValidPassword) {
+            return res.status(400).json({error: "Invalid username or password"})
+        }
+        // store user id in a session so user doesnt have to continue to login
+        req.session.userId = user.id;
+        res.json({message: "login successful"})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: "Something went wrong during login!"})
+    }
+})
+
+// check if a user is logged in
+router.get("/me", async(req: Request, res: Response) => {
+    // const userId = req.session.userId;
+    // if (!userId) {
+    //     return res.status(401).json({error: "Not authenticated"})
+    // }
+    if (!req.session.userId) {
+        return res.status(401).json({message: "Not logged in" })
+    }
+    const user = await prisma.user.findUnique({
+        where: {id: req.session.userId},
+        select: {username: true} // only return necessary data about the user (the username)
+    })
+
+    res.json({ id: req.session.userId, username: user.username })
+})
+
+// when a user logs in creates a session
+// session id stored in users browser as cookie
+// each request sends cookie to server to keep user logged in
 
 module.exports = router
