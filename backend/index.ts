2b3d900d98c9300758d467772ebbb32cf21c1eea
Brianna Flores <briannaflores@meta.com>
2025-06-27 13:47:26 -0700

    this commit will remove unneccesary comments

--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,11 @@
-// get the types for defining req and resAdd commentMore actions
 import { Request, Response } from  "express";
 const express = require('express');
 const app = express();
 const PORT = 3000;
 const cors = require('cors')
 app.use(cors())
-// app.use(express.json())
+app.use(express.json())
 
-
-// import prisma
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
@@ -36,30 +33,3 @@ app.listen(PORT, () => {
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to my app!')
 })
-
-// // modify the express session module to allow extraction of userId from session in auth.ts
-// declare module 'express-session' {
-//   interface SessionData {
-//     userId?: string
-//   }
-// }
-// const session = require('express-session')
-
-// // takes in json format
-
-
-// // allow to import data from .env file
-// import dotenv from 'dotenv'; 
-// dotenv.config();
-
-// let sessionConfig = {
-//   name: 'sessionId',
-//   secret: process.env.SESSION_SECRET, 
-//   cookie: {
-//     maxAge: 1000 * 60 * 5,
-//     secure: process.env.RENDER ? true : false,
-//   }
-// }
-
-// app.use(session(sessionConfig))
-
