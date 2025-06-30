ac9c81795b1d425f643a6ccead6f4ea83caa9b1d
Brianna Flores <briannaflores@meta.com>
2025-06-29 21:54:31 -0700

    this commit will style the add ingredient modal

--- frontend/src/pages/IngredientsPage.tsx
+++ frontend/src/pages/IngredientsPage.tsx
@@ -1,18 +1,24 @@
 import "../styles/IngredientsPage.css";
 import type { RecipeToggleNavBar } from "../utils/types";
 import AppHeader from "../components/AppHeader";
-
+import { v4 as uuidv4 } from "uuid";
+import { useState } from 'react'
 import {ingredients} from "../utils/sampleData"
 import Ingredient from "../components/Ingredient";
 import AddIngredientModal from "../components/AddIngredientModal";
-import { v4 as uuidv4 } from "uuid";
 
 const IngredientsPage = ({ navOpen, toggleNav }: RecipeToggleNavBar) => {
+  const [addIngredientModalOpen, setAddIngredientModalOpen] = useState(false)
+
+  const addIngredientClick = () => {
+    setAddIngredientModalOpen((prev) => !prev)
+  }
+
   return (
     <div className="ingredients-page">
       <AppHeader navOpen={navOpen} toggleNav={toggleNav}/>
       <section className="ingredient-page-container">
-        <button className="add-ingredient-button">Add Ingredient</button>
+        <button className="add-ingredient-button" onClick={addIngredientClick}>Add Ingredient</button>
         <div className="ingredient-columns">
           <h3>Ingredient</h3>
           <h3>Quantity</h3>
@@ -28,7 +34,7 @@ const IngredientsPage = ({ navOpen, toggleNav }: RecipeToggleNavBar) => {
           }
         </div>
       </section>
-      <AddIngredientModal />
+      {addIngredientModalOpen && <AddIngredientModal onClose={addIngredientClick} />}
     </div>
   );
 };
