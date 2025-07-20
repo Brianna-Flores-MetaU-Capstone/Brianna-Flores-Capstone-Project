3c93114ea3c871372eba1d98005dd5d0f0b50209
Brianna Flores <briannaflores@meta.com>
2025-07-20 16:44:40 -0700

    this commit will adapt the longest common subseuqence algorithm to be utilized in an array of strings

--- frontend/src/pages/RecipeDiscoveryPage.tsx
+++ frontend/src/pages/RecipeDiscoveryPage.tsx
@@ -1,5 +1,5 @@
 import AppHeader from "../components/AppHeader";
-import { Box } from "@mui/joy";
+import { Box, Button } from "@mui/joy";
 import { useState, useEffect } from "react";
 import type {
   GPErrorMessageTypes,
@@ -23,6 +23,7 @@ import ErrorState from "../components/ErrorState";
 import MealInfoModal from "../components/MealInfoModal";
 import { useUser } from "../contexts/UserContext";
 import EditRecipeModal from "../components/EditRecipeModal";
+import { getLCS } from "../utils/diffUtils";
 
 
 // https://www.typescriptlang.org/docs/handbook/utility-types.html#recordkeys-type
--- frontend/src/utils/diffUtils.ts
+++ frontend/src/utils/diffUtils.ts
@@ -2,14 +2,13 @@ import type { GPIngredientDataTypes, GPRecipeDataTypes } from "./types";
 import { RecipeIngredientsDiff } from "../classes/DiffClass";
 import type { GPDiffReturnType } from "../classes/DiffClass";
 
-
 type GPRecipeDiffType = {
-  recipeA: GPRecipeDataTypes
-  recipeB: GPRecipeDataTypes
-  servingsDiff: boolean
-  recipeIngredientDiff: GPDiffReturnType<GPIngredientDataTypes>
-  purchasedIngredientsDiff: GPDiffReturnType<GPIngredientDataTypes>
-}
+  recipeA: GPRecipeDataTypes;
+  recipeB: GPRecipeDataTypes;
+  servingsDiff: boolean;
+  recipeIngredientDiff: GPDiffReturnType<GPIngredientDataTypes>;
+  purchasedIngredientsDiff: GPDiffReturnType<GPIngredientDataTypes>;
+};
 type GPDiffType = {
   recipeA: GPRecipeDataTypes;
   recipeB: GPRecipeDataTypes;
@@ -25,16 +24,71 @@ const getRecipeDiffResults = ({ recipeA, recipeB }: GPDiffType) => {
     recipeA.ingredientCostInfo,
     recipeB.ingredientCostInfo
   );
-  const diffServingsResults = recipeA.servings !== recipeB.servings
+  const diffServingsResults = recipeA.servings !== recipeB.servings;
   const recipeDiffResults: GPRecipeDiffType = {
     recipeA,
     recipeB,
     servingsDiff: diffServingsResults,
     recipeIngredientDiff: diffRecipeIngredientsResults,
-    purchasedIngredientsDiff: diffIngredientsToPurchaseResults
-  }
-  return recipeDiffResults
+    purchasedIngredientsDiff: diffIngredientsToPurchaseResults,
+  };
+  return recipeDiffResults;
 };
 
-export { getRecipeDiffResults };
-export type { GPRecipeDiffType }
+const getLCS = ({ recipeA, recipeB }: GPDiffType) => {
+  const instructionsA = recipeA.instructions;
+  const instructionsB = recipeB.instructions;
+  const n = instructionsA.length;
+  const m = instructionsB.length;
+
+  const dp = new Array(n + 1).fill(null).map(() => new Array(m + 1).fill(0));
+  for (let i = 0; i <= n; i++) {
+    dp[i][0] = 0;
+  }
+  for (let i = 0; i <= m; i++) {
+    dp[0][i] = 0;
+  }
+
+  for (let ind1 = 1; ind1 <= n; ind1++) {
+    for (let ind2 = 1; ind2 <= m; ind2++) {
+      // Dont take into consideration capitalization differences, to lowercase
+      if (
+        instructionsA[ind1 - 1].toLowerCase() ===
+        instructionsB[ind2 - 1].toLowerCase()
+      ) {
+        dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+      } else {
+        dp[ind1][ind2] = Math.max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+      }
+    }
+  }
+
+  // reconstruct
+  const len = dp[n][m];
+  let i = n;
+  let j = m;
+  let index = len - 1;
+  let unchanged: string[] = [];
+  let added: string[] = [];
+  let deleted: string[] = []
+
+  while (i > 0 && j > 0) {
+    if (instructionsA[i - 1] === instructionsB[j - 1]) {
+      unchanged = [instructionsA[i - 1], ...unchanged];
+      index--;
+      i--;
+      j--;
+    } else if (dp[i - 1][j] > dp[i][j - 1]) {
+      deleted = [instructionsA[i - 1], ...deleted]
+      i--;
+    } else {
+      added = [instructionsB[j - 1], ...added]
+      j--;
+    }
+  }
+  // array of unchanged lines
+  return unchanged;
+};
+
+export { getRecipeDiffResults, getLCS };
+export type { GPRecipeDiffType };
