62a7f919d05f5c87e9794cc2b4f099a94b042652
Brianna Flores <briannaflores@meta.com>
2025-07-22 00:07:02 -0700

    this commit will refactor out individual methods to get two recipes individual diff categories

--- frontend/src/classes/recipeDiffClasses/DiffRecipes.ts
+++ frontend/src/classes/recipeDiffClasses/DiffRecipes.ts
@@ -11,37 +11,68 @@ class DiffRecipes {
     this.recipeB = recipeB;
   }
 
-  getFullRecipeDiff() {
+  getInstructionDiff() {
     const diffInstructions = new DiffRecipeStringArray(
       this.recipeA.instructions,
-      this.recipeB.instructions,
+      this.recipeB.instructions
     );
     const instructionsDiffResults = diffInstructions.getStringArrayDiff();
+    return instructionsDiffResults;
+  }
+
+  getIngredientsDiff() {
     const diffIngredients = new DiffRecipeIngredients(
       this.recipeA.ingredients,
-      this.recipeB.ingredients,
+      this.recipeB.ingredients
     );
     const ingredientsDiffResults = diffIngredients.getIngredientsDiff();
+    return ingredientsDiffResults;
+  }
+
+  getTitleDiff() {
     const diffTitle = new DiffRecipeStringArray(
       [this.recipeA.recipeTitle],
-      [this.recipeB.recipeTitle],
+      [this.recipeB.recipeTitle]
     );
     const titleDiffResults = diffTitle.getStringArrayDiff();
+    return titleDiffResults;
+  }
+
+  getServingsDiff() {
     const diffServings = new DiffRecipeStringArray(
       [this.recipeA.servings.toString()],
-      [this.recipeB.servings.toString()],
+      [this.recipeB.servings.toString()]
     );
     const servingsDiffResults = diffServings.getStringArrayDiff();
+    return servingsDiffResults;
+  }
+
+  getTagDiff() {
     const diffTags = new DiffRecipeStringArray(
       this.recipeA.recipeTags,
-      this.recipeB.recipeTags,
+      this.recipeB.recipeTags
     );
     const tagsDiffResults = diffTags.getStringArrayDiff();
+    return tagsDiffResults;
+  }
+
+  getCookTimeDiff() {
     const diffCookTime = new DiffRecipeStringArray(
       [this.recipeA.readyInMinutes.toString()],
-      [this.recipeB.readyInMinutes.toString()],
+      [this.recipeB.readyInMinutes.toString()]
     );
     const cookTimeDiffResults = diffCookTime.getStringArrayDiff();
+    return cookTimeDiffResults;
+  }
+
+  getFullRecipeDiff() {
+    const instructionsDiffResults = this.getInstructionDiff();
+    const ingredientsDiffResults = this.getIngredientsDiff();
+    const titleDiffResults = this.getTitleDiff();
+    const servingsDiffResults = this.getServingsDiff();
+    const tagsDiffResults = this.getTagDiff();
+    const cookTimeDiffResults = this.getCookTimeDiff();
+
     return {
       instructionsDiffResults,
       ingredientsDiffResults,
